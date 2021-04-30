7c9412a5c65c78a2ff722c11fee55d321abc0116
xxpic <xxpickle.overlordxx@gmail.com>
2021-04-30 19:02:49 -0400

    Full Flappy Bird Game

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import keyboard as k
 import time
+import random as ra
 
 maxHeight = 10
 minHeight = -10
-width = 100
+width = 60
 padding = 30
-speed = 20
-grav = -8
-jumpHeight = 8
+speed = 30
+xPos = 5.0
+grav = -45
+pipex = 100
+PipeCenter = 0
+PipeSize = 4
+jumpHeight = 16
 termVel = -4
-debug = False
-FPS = 30
+debug = True
+FPS = 0
 
 
 
 ySpeed = 0
 lastFrameTime = time.time()
-points = [(5.0,0.0)]
+points = [(xPos,0.0)]
+playing = True
+score = 0
+canScore = True
 
 def render(pl):
     finalString = ""
@@ -34,10 +42,11 @@ def render(pl):
             curLine += curChar
         finalString += (curLine + "\n")
     if debug:
-        print("\r" + (('\n'*padding)+finalString + str(points[0])), end="")
+        debugInfo = "pos: " + str(points[0]) + "\n" + "vel: " + str(ySpeed)
+        print("\r" + (('\n'*padding) + debugInfo + "\n" + ("Score: " + str(score)+ "\n") + finalString), end="")
     else:
         # https://stackoverflow.com/questions/465348/how-can-i-overwrite-print-over-the-current-line-in-windows-command-line
-        print("\r" + (('\n'*padding)+finalString), end="")
+        print("\r" + (('\n'*padding) + ("Score: " + str(score) + "\n") + finalString), end="")
 
 
 def AddTuple(t, x, y):
@@ -52,8 +61,40 @@ def SetTuple(t, x, y):
     t[1] = y
     return tuple(t)
 
+def line(x, y, b):
+    global points
+    add_points = []
+    if b:
+        for i in range(minHeight, y):
+            add_points.append((x, i))
+    else:
+        for i in range(maxHeight, y, -1):
+            add_points.append((x, i))
+    return add_points
+
+def pipe(s, c, x):
+    return line(x, int(c - s / 2), True) + line(x, int(c + s / 2), False)
+
+def GameOver():
+    global playing
+    playing = False
+
+    print("Game Over!")
+    print("Your score was: " + str(score))
+    input()
+
+def Score():
+    global score
+    global canScore
+    if canScore:
+        score += 1
+        canScore = False
 def main(deltatime):
     global ySpeed
+    global points
+    global pipex
+    global PipeCenter
+    global canScore
 
     points[0] = AddTuple(points[0], 0, 0.5 * ySpeed * deltatime)
 
@@ -63,10 +104,36 @@ def main(deltatime):
     ySpeed += grav * deltatime
 
     points[0] = AddTuple(points[0], 0, 0.5 * ySpeed * deltatime)
+
+    pipex -= speed * deltatime
+    pipeInstince = pipe(PipeSize, PipeCenter, int(pipex))
+    points += pipeInstince
+
+    if pipex <= 0:
+        canScore = True
+        pipex = width + 1
+        PipeCenter = ra.randint(minHeight + PipeSize, maxHeight-PipeSize)
+    if int(pipex) == xPos:
+        Score()
+
     render(points)
 
+    for i in pipeInstince:
+        if int(i[0]) == int(points[0][0]) and int(i[1]) == int(points[0][1]):
+            GameOver()
 
-while True:
+        points.remove(i)
+
+    if points[0][1] >= maxHeight:
+        points[0] = SetTuple(points[0], xPos, maxHeight)
+
+    if points[0][1] <= minHeight:
+        GameOver()
+
+
+
+
+while playing:
     #gets deltatime
     currentTime = time.time()
     dt = currentTime - lastFrameTime
@@ -81,11 +148,3 @@ while True:
 
     #calls main function
     main(dt)
-
-    #prints fps
-    if debug:
-        try:
-            CurrentFps = 1 / (time.time() - currentTime)
-            print(CurrentFps)
-        except ZeroDivisionError:
-            print('Divide by 0')
